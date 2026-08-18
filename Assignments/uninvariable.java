ab440ebb9ab4eecc890c809a6639bdaef36bd677
Rohitpatil2-hash <rohit1.patil.cmaug26@gmail.com>
2026-08-18 22:45:22 +0530

    Add files via upload

---
+++ Assignments/uninvariable.java
@@ -0,0 +1,22 @@
+public class Uninvariable{
+byte a; 
+short b; 
+int c; 
+long d; 
+float e; 
+double f; 
+char g; 
+boolean h;
+public static void main(String args[]){
+Uninvariable viv=new Uninvariable();
+System.out.println(viv.a);
+System.out.println(viv.b);
+System.out.println(viv.c);
+System.out.println(viv.d);
+System.out.println(viv.e);
+System.out.println(viv.f);
+System.out.println(viv.g);
+System.out.println(viv.h);
+
+}
+}
