21a1f3e9336f0c0b6aee142de79ab4adcb099205
Rohitpatil2-hash <rohit1.patil.cmaug26@gmail.com>
2026-09-16 16:14:25 +0530

    Add files via upload

---
+++ Assignments/OOPS JAVA/ForLoops Exercise/Ques8.java
@@ -0,0 +1,14 @@
+
+public class Ques8 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=5;i++) {
+			for(int j=1;j<=6-i;j++) {
+				System.out.print(j +" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
